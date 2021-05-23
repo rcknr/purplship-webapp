4db744a2eb553a3ea9624b0971c5a10d86d7ed49
DanH91 <danielk.developer@gmail.com>
2021-05-23 05:42:59 -0400

    fix token regeneration action

--- components/generate-api-dialog.tsx
+++ components/generate-api-dialog.tsx
@@ -8,33 +8,24 @@ interface GenerateAPIModalComponent {}
 // TODO: Handle password
 const GenerateAPIModal: React.FC<GenerateAPIModalComponent> = TokenMutation<GenerateAPIModalComponent>(({ children, updateToken }) => {
     const { notify } = useContext(Notify);
-    const [password, setPassword] = useState<string>("");
-    const [error, setError] = useState<string>("");
     const [isActive, setIsActive] = useState<boolean>(false);
     const [isDisabled, setIsDisabled] = useState<boolean>(false);
-    const [hasError, setHasError] = useState<boolean>(false);
     const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
         evt.preventDefault();
         try {
             setIsDisabled(true);
             await updateToken({ refresh: true });
-            setPassword("");
             setIsDisabled(false);
             setIsActive(false);
-            setHasError(false);
             notify({ type: NotificationType.success, message: "New token generated successfully!" });
         } catch(err) {
-            setError(err.message);
             setIsDisabled(false);
-            setHasError(true);
         }
     };
     const close = (evt: React.MouseEvent) => {
         evt.preventDefault();
-        setPassword("");
         setIsDisabled(false);
         setIsActive(false);
-        setHasError(false);
     }
 
     return (
@@ -48,17 +39,7 @@ const GenerateAPIModal: React.FC<GenerateAPIModalComponent> = TokenMutation<Gene
                 <form className="modal-card" onSubmit={handleSubmit}>
                     <section className="modal-card-body">
                         <div className="field">
-                            <label className="label">Enter your password</label>
-                            <div className="control">
-                                <input
-                                    className="input is-small"
-                                    type="password"
-                                    onChange={e => setPassword(e.target.value)}
-                                    value={password}
-                                    disabled={isDisabled}
-                                    required />
-                            </div>
-                            <p className="is-size-7 has-text-danger my-1" style={{ visibility: (hasError ? "visible" : "hidden")}}>{error}</p>
+                            <label className="label">Generate a new API Token</label>
                             <input className="button is-small is-fullwidth mt-2" type="submit" value="Submit" disabled={isDisabled} />
                         </div>
                     </section>
