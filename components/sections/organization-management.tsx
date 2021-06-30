db2337189a91f1fc7b5193ec8e9f2fec5b549763
Daniel Kobina <danielk.developer@gmail.com>
2021-06-30 06:44:05 -0400

    clean up org mgmt component formatting

--- components/sections/organization-management.tsx
+++ components/sections/organization-management.tsx
@@ -1,27 +1,27 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import OrganizationUpdateInput from '@/components/organization-update-input';
 
 interface OrganizationManagementComponent { }
 
-const OrganizationManagement: React.FC<OrganizationManagementComponent> = ({  }) => {
+const OrganizationManagement: React.FC<OrganizationManagementComponent> = ({ }) => {
 
-        return (
-            <>
+  return (
+    <>
 
-                <div className="columns py-6">
-                    <div className="column is-5 pr-6">
-                        <p className="subtitle is-6 py-1">Organization</p>
-                    </div>
+      <div className="columns py-6">
+        <div className="column is-5 pr-6">
+          <p className="subtitle is-6 py-1">Organization</p>
+        </div>
 
-                    <div className="column is-7">
+        <div className="column is-7">
 
-                        <OrganizationUpdateInput label="name" propertyKey="name" inputType="text" />
+          <OrganizationUpdateInput label="name" propertyKey="name" inputType="text" />
 
-                    </div>
-                </div>
+        </div>
+      </div>
 
-            </>
-        )
-    };
+    </>
+  )
+};
 
 export default OrganizationManagement;
