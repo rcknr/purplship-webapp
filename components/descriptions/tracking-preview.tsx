78e272c77d7c5f100007786be420545c7230f64a
DanH91 <danielk.developer@gmail.com>
2021-07-24 20:30:55 -0400

    fix tracking preview link state

--- components/descriptions/tracking-preview.tsx
+++ components/descriptions/tracking-preview.tsx
@@ -14,36 +14,43 @@ export const TrackingPreviewContext = React.createContext<TrackingPreviewContext
 
 const TrackingPreview: React.FC<TrackingPreviewComponent> = ({ children }) => {
     const link = useRef<HTMLAnchorElement>(null);
-    const linkShare = useRef<HTMLInputElement>(null);
     const [isActive, setIsActive] = useState<boolean>(false);
+    const [sharingLink, setSharingLink] = useState<string>('');
     const [key, setKey] = useState<string>(`tracker-${Date.now()}`);
     const [tracker, setTracker] = useState<TrackingStatus>();
 
     const previewTracker = (tracker: TrackingStatus) => {
         setTracker(tracker);
         setIsActive(true);
+        setKey(`tracker-${Date.now()}`);
+        link.current?.setAttribute('href', `/tracking/${tracker.id}`);
+        setSharingLink(link.current?.href as string);
     };
     const dismiss = (_?: React.MouseEvent) => {
-        setKey(`tracker-${Date.now()}`);
         setIsActive(false);
         setTracker(undefined);
+        setKey(`tracker-${Date.now()}`);
     };
     const copy = (_: React.MouseEvent) => {
-        linkShare.current?.select();
-        document.execCommand("copy");
+        var input = document.createElement('input');
+        input.setAttribute('value', sharingLink);
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand('copy');
+        document.body.removeChild(input);
     };
     const computeColor = (tracker: TrackingStatus) => {
-        if (tracker.delivered) return "has-background-success";
-        else if (tracker.status === ListStatusEnum.Pending.toString()) return "has-background-grey-dark";
+        if (tracker?.delivered) return "has-background-success";
+        else if (tracker?.status === ListStatusEnum.Pending.toString()) return "has-background-grey-dark";
         else return "has-background-info";
     };
     const computeStatus = (tracker: TrackingStatus) => {
-        if (tracker.delivered) return "Delivered";
-        else if (tracker.status === ListStatusEnum.Pending.toString()) return "Pending";
+        if (tracker?.delivered) return "Delivered";
+        else if (tracker?.status === ListStatusEnum.Pending.toString()) return "Pending";
         else return "In-Transit";
     };
     const computeEvents = (tracker: TrackingStatus): DayEvents => {
-        return (tracker.events || []).reduce((days, event: TrackingEvent) => {
+        return (tracker?.events || []).reduce((days, event: TrackingEvent) => {
             const daydate = new Date(event.date as string).toUTCString().split(' ').slice(0, 4).join(' ');
             return { ...days, [daydate]: [...(days[daydate] || []), event] };
         }, {} as DayEvents);
@@ -56,6 +63,7 @@ const TrackingPreview: React.FC<TrackingPreviewComponent> = ({ children }) => {
             </TrackingPreviewContext.Provider>
 
             <div className={`modal ${isActive ? "is-active" : ""}`} key={key}>
+                <a ref={link}></a>
                 <div className="modal-background" onClick={dismiss}></div>
 
                 {!isNone(tracker) && <div className="modal-card">
@@ -104,12 +112,13 @@ const TrackingPreview: React.FC<TrackingPreviewComponent> = ({ children }) => {
                                 <label className="label">Share with customer</label>
                                 <input
                                     className="input is-small" type="text" title="Click to Copy"
-                                    defaultValue={link.current?.href} ref={linkShare} style={{ width: '80%' }}
+                                    value={sharingLink}
+                                    style={{ width: '80%' }}
                                     readOnly />
                                 <button className="button is-small is-light mx-1" onClick={copy}>
                                     <span className="icon is-small"><i className="fas fa-copy"></i></span>
                                 </button>
-                                <a className="button is-small is-light" href={`/tracking/${tracker?.id}`} ref={link} target="blank">
+                                <a className="button is-small is-light" href={sharingLink} target="blank">
                                     <span className="icon is-small"><i className="fas fa-share-square"></i></span>
                                 </a>
                             </div>
